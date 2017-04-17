461142370a0b5b9a87ad8403edc0bb4d9636da2f
Tobiasz12 <tobiasz63@op.pl>
2017-04-17 20:19:19 +0200

    zadanie1

--- script.js
+++ script.js
@@ -3,16 +3,21 @@ function Telefon(marka, cena, kolor,pamięć_Ram) {
 	this.cena = cena;
 	this.kolor = kolor;
 	this.pamięć_Ram = pamięć_Ram;
+	this.getWarranyCost = function(){
+		console.log("ceny przedłużonej gwarancji równa 10% ceny telefonu wynosi: " + (this.cena * 0.90 ))
+	}
 }
 
 Telefon.prototype.printInfo = function() {
-	console.log("Marka telefonu to " + this.marka + ", kolor to " + this.kolor + ", a cena to " + this.cena +", do tego pamięć: " + this.pamięć_Ram + ".");
+	console.log("Marka telefonu to " + this.marka + ", kolor to " + this.kolor + ", a cena to " + this.cena +", do tego pamięć: " + this.pamięć_Ram + ".");	
+	this.getWarranyCost();
 }
 
-var iPhone6S = new Telefon("Apple", 2250, "srebrny", " 4 GB");
+var IPhone6S = new Telefon("Apple", 2250, "srebrny", " 4 GB");
 var Samsung_Galaxy_S6 = new Telefon("Samsung", 3499,"orchild grey"," 5 GB" );
 var OnePlus_One = new Telefon("OnePlus", 899, "red"," 6 GB");
 
-iPhone6S.printInfo();
+
+IPhone6S.printInfo();
 Samsung_Galaxy_S6.printInfo();
 OnePlus_One.printInfo();
